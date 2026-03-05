fb39f47c8b648efd998f8f1d269cd9dea12ba578
GuustavoPetry <ogustavopetry@gmail.com>
2026-03-05 06:58:36 -0300

    Subindo banco de dados com Prisma + PostgreSQL + Docker Compose

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
-import fastify = require("fastify");
+import fastify from "fastify";
+import { PrismaClient }  from "@prisma/client";
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data: {
+        name: "Gustavo Petry",
+        email: "ogustavopetry@gmail.com"
+    }
+});
 
