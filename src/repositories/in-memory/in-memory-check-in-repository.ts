662e22679fec7150c05a12b7122c7f8444cc39dc
GuustavoPetry <ogustavopetry@gmail.com>
2026-04-09 15:40:01 -0300

    In memory repositories from test

--- src/repositories/in-memory/in-memory-check-in-repository.ts
+++ src/repositories/in-memory/in-memory-check-in-repository.ts
@@ -2,6 +2,7 @@ import { Prisma, CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "../check-ins-repository";
 import { randomUUID } from "node:crypto";
 import dayjs from "dayjs";
+import { check } from "zod";
 
 export class InMemoryCheckInRepository implements CheckInsRepository {
     public items: CheckIn[] = []
@@ -24,6 +25,26 @@ export class InMemoryCheckInRepository implements CheckInsRepository {
         return checkInOnSameDate;
     }
 
+    async findById(id: string) {
+        const checkIn = this.items.find((item) => item.id === id);
+
+        if (!checkIn) {
+            return null;
+        }
+
+        return checkIn
+    }
+
+    async findManyByUserId(userId: string, page: number) {
+        return this.items
+            .filter(item => item.user_id === userId)
+            .slice((page - 1) * 20, page * 20);
+    }
+
+    async countByUserId(userId: string) {
+        return this.items.filter((item) => item.user_id === userId).length;
+    }
+
     async create(data: Prisma.CheckInUncheckedCreateInput) {
         const checkIn = {
             id: randomUUID(),
@@ -34,6 +55,16 @@ export class InMemoryCheckInRepository implements CheckInsRepository {
         }
 
         this.items.push(checkIn);
+
+        return checkIn;
+    }
+
+    async save(checkIn: CheckIn) {
+        const checkInIndex = this.items.findIndex(item => item.id === checkIn.id);
+
+        if (checkInIndex >= 0) {
+            this.items[checkInIndex] = checkIn;
+        }
 
         return checkIn;
     }
