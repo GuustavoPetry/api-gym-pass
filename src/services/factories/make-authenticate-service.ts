f08b13b03d1c350d73c107b7bd8140f5ceffc947
GuustavoPetry <ogustavopetry@gmail.com>
2026-04-14 06:19:28 -0300

    Factories

--- src/services/factories/make-authenticate-service.ts
+++ src/services/factories/make-authenticate-service.ts
@@ -1,9 +1,9 @@
-import { PrismaUserRepository } from "@/repositories/prisma/prisma.user.repository";
+import { PrismaUserRepository } from "@/repositories/prisma/prisma-user-repository";
 import { AuthenticateService } from "../authenticate.service";
 
 export function makeAuthenticateService() {
     const usersRepository = new PrismaUserRepository();
-    const authenticateService = new AuthenticateService(usersRepository);
+    const service = new AuthenticateService(usersRepository);
 
-    return authenticateService;
+    return service;
 }
--- src/services/factories/make-register-service.ts
+++ src/services/factories/make-register-service.ts
@@ -1,9 +1,9 @@
-import { PrismaUserRepository } from "@/repositories/prisma/prisma.user.repository";
+import { PrismaUserRepository } from "@/repositories/prisma/prisma-user-repository";
 import { RegisterService } from "../register.service";
 
 export function makeRegisterService() {
     const usersRepository = new PrismaUserRepository();
-    const registerService = new RegisterService(usersRepository);
+    const service= new RegisterService(usersRepository);
 
-    return registerService;
+    return service;
 }
