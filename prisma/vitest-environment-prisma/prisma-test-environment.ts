bedf151d598f5eeadf50ee724f226e960dee681c
GuustavoPetry <ogustavopetry@gmail.com>
2026-04-21 12:39:45 -0300

    testEnvironment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,35 +1,42 @@
-import { config } from "dotenv";
+import { prisma } from "@/lib/prisma";
 import { execSync } from "node:child_process";
-import { Client } from "pg";
+import { randomUUID } from "node:crypto";
 import type { Environment } from "vitest/environments";
 
-config({ path: ".env.test", override: true });
+function generateDatabaseUrl(schema: string) {
+    if (!process.env.DATABASE_URL) {
+        throw new Error("Please provide a DATABASE_URL env variable");
+    }
+
+    const url = new URL(process.env.DATABASE_URL);
+
+    url.searchParams.set("schema", schema);
+
+    return url.toString();
+}
 
 export default <Environment>{
     name: "prisma",
     viteEnvironment: "ssr",
     async setup() {
         // aplica migrations no banco de teste
-        execSync("npx prisma migrate deploy", {
-            env: {
-                ...process.env,
-                DATABASE_URL: process.env.DATABASE_URL!,
-            },
-            stdio: "inherit",
-        });
+        const schema = randomUUID();
+        const databaseUrl = generateDatabaseUrl(schema);
+
+        console.log(databaseUrl);
+
+        process.env.DATABASE_URL = databaseUrl;
+
+        execSync("npx prisma migrate deploy");
 
         return {
             async teardown() {
                 // limpa todas as tabelas após cada arquivo de teste
-                const client = new Client({
-                    connectionString: process.env.DATABASE_URL,
-                });
+                await prisma.$executeRawUnsafe(
+                    `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+                );
 
-                await client.connect();
-                await client.query(`
-                    TRUNCATE TABLE users, check_ins, gyms RESTART IDENTITY CASCADE
-                `);
-                await client.end();
+                await prisma.$disconnect();
             },
         };
     },
